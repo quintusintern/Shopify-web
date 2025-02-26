efedc6e2123c60cb5e233a85f7165b1afd76b3c6
Amul Sharma <166657014+Sharmaamul89@users.noreply.github.com>
2025-02-26 16:59:33 +0530

    Blog Section Improved

--- src/components/BlogSection/index.jsx
+++ src/components/BlogSection/index.jsx
@@ -1,4 +1,4 @@
-import styles from './BlogSection.module.css'
+import styles from "./BlogSection.module.css";
 
 const BlogSection = () => {
   const blogPosts = [
@@ -14,7 +14,7 @@ const BlogSection = () => {
     {
       id: 2,
       image: "/Top.jpg",
-      title: "The Easiest Way to Break Out on Top",
+      title: "The Easi0est Way to Break Out on Top",
       date: "May 11, 2022",
       author: "admin",
       description:
@@ -33,17 +33,29 @@ const BlogSection = () => {
 
   return (
     <div className={styles.blogSection}>
-      <h2 className={styles.blogHeading}>LATES FROM BLOG</h2>
-      <p className={styles.blogSubheading}>The freshest and most exciting news</p>
-
+      <h2 className={styles.blogHeading}>LATEST FROM BLOG</h2>
+      <p className={styles.blogSubheading}>
+        The freshest and most exciting news
+      </p>
       <div className={styles.blogContainer}>
         {blogPosts.map((post) => (
           <div key={post.id} className={styles.blogCard}>
-            <div className={styles.imgContainer}>
-            <img src={post.image} alt={post.title} className={styles.blogImage} />
+            <div className={styles.parentContainer}>
+              <div className={styles.imageContainer}>
+                <div className={styles.effectOverlay}>
+                  <div className={`${styles.borderLine} ${styles.top}`}></div>
+                  <div className={`${styles.borderLine} ${styles.right}`}></div>
+                  <div className={`${styles.borderLine} ${styles.bottom}`}></div>
+                  <div className={`${styles.borderLine} ${styles.left}`}></div>
+                </div>
+                <img src={post.image} alt={post.title} />
+              </div>
             </div>
+
             <h3 className={styles.blogTitle}>{post.title}</h3>
-            <p className={styles.blogMeta}>By {post.author} on {post.date}</p>
+            <p className={styles.blogMeta}>
+              By {post.author} on {post.date}
+            </p>
             <p className={styles.blogDescription}>{post.description}</p>
           </div>
         ))}

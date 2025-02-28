ce3b7956098e444fce832ac6d44c44a1639e4a33
Assistantakku234 <114659735+Assistantakku234@users.noreply.github.com>
2025-02-28 12:58:27 +0530

    Footer

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -8,22 +8,21 @@ const ProductsPage = () => {
     <div className={styles.container}>
       {/* Header Navigation */}
       <nav className={styles.navbar}>
-        <ul>
-          <li><Link href="#">New Arrival</Link></li>
-          <li><Link href="#">Decor</Link></li>
-          <li><Link href="#">Denim</Link></li>
-          <li><Link href="#">Dress</Link></li>
-          <li><Link href="#">Hats</Link></li>
-          <li><Link href="#">Men</Link></li>
-          <li><Link href="#">Sale</Link></li>
-          <li><Link href="#">Shoes</Link></li>
-          <li><Link href="#">Women</Link></li>
+        <ul className={styles.navList}>
+          <li className={styles.newArrival}><Link className={styles.navLink} href="#">New Arrival</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Decor</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Denim</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Dress</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Hats</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Men</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Sale</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Shoes</Link></li>
+          <li className={styles.navItem}><Link className={styles.navLink} href="#">Women</Link></li>
         </ul>
       </nav>
 
-     
-     
-    </div>
+      
+    </div> 
   );
 };
 
